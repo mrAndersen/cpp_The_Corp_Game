d650c241c5da6a19db50ac01adf3cf882149530b
mrandersen2004@yandex.ru <mrandersen2004@yandex.ru>
2017-09-08 13:16:00 +0300

    +

---
+++ src/World/World.h
@@ -0,0 +1,19 @@
+#ifndef THE_CORP_GAME_WORLD_H
+#define THE_CORP_GAME_WORLD_H
+
+#include <string>
+
+class World
+{
+    int width = 50000;
+    int height = 6000;
+
+    std::string title = "New brave commercial world";
+
+public:void init();
+
+
+};
+
+
+#endif //THE_CORP_GAME_WORLD_H
