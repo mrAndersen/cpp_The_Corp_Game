3032abf22bb28e56aa376244f0d51ebe87d4eb01
mrAndersen <mrandersen2004@yandex.ru>
2017-11-13 00:55:22 +0300

    +

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
     System::initWindow();
     System::initDebug();
 
-//    EntityContainer::initBackground();
+    EntityContainer::initBackground();
     EntityContainer::initGrid();
     ControlPanel::initControlPanel();
 
