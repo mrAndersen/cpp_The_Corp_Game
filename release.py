a4889c4243db21ac08133350bdc216371e0b39de
mrandersen2004@yandex.ru <mrandersen2004@yandex.ru>
2017-11-28 18:37:11 +0300

    +

--- release.py
+++ release.py
@@ -5,21 +5,23 @@ import argparse
 import http.client
 import json
 import github
+import requests
+import base64
 
-parser = argparse.ArgumentParser(description='cpp Deployer')
-parser.add_argument('--zip', help='Zip release and put in root directory', action='store_true')
-parser.add_argument('--rm', help='Remove folder after creating zip archive', action='store_true')
-parser.add_argument('--upload', help="Upload to github", action='store_true')
+# parser = argparse.ArgumentParser(description='cpp Deployer')
+# parser.add_argument('--zip', help='Zip release and put in root directory', action='store_true')
+# parser.add_argument('--rm', help='Remove folder after creating zip archive', action='store_true')
+# parser.add_argument('--upload', help="Upload to github", action='store_true')
 
-args = parser.parse_args()
+# args = parser.parse_args()
+# print("Working zip = {}, rm = {}, upload = {}", args.zip, args.rm, args.upload)
 
-print("Working zip = {}, rm = {}, upload = {}", args.zip, args.rm, args.upload)
+print("Copying...", end="")
 
 binGccDirector = "cmake-build-debug"
 binMsvcDirectory = "Debug"
 
 timeString = time.strftime("%H-%M-%S_%d-%B", time.localtime())
-
 releaseDirectory = "release_" + timeString
 
 if not os.path.exists(releaseDirectory):
@@ -37,15 +39,39 @@ shutil.copyfile("./" + binMsvcDirectory + "/cpp_The_Corp_Game_VC.exe", releaseDi
 # manual
 shutil.copyfile("./manual.txt", releaseDirectory + "/manual.txt")
 
-g = github.Github("mrAndersen", "matrixx1s")
-release = g.get_user().get_repo("cpp_The_Corp_Game").create_git_release(tag="early-alpha", message=timeString, name="early-alpha")
+print("\r Copying... Done")
 
-print(release.upload_url)
+if True:
+    print("Zipping...", end="")
+    shutil.make_archive(releaseDirectory, 'zip', releaseDirectory)
+    size = os.path.getsize(releaseDirectory + ".zip")
+    print("\r Zipping... " + str(round(size / 1024 / 1024, 2)) + " Mb")
 
-# if args.zip:
-#     shutil.make_archive(releaseDirectory, 'zip', releaseDirectory)
-#     size = os.path.getsize(releaseDirectory + ".zip")
-#     print("zipped = " + str(round(size / 1024 / 1024, 2)) + " Mb")
-#
-# if args.rm:
-#     shutil.rmtree(releaseDirectory)
+if True:
+    shutil.rmtree(releaseDirectory)
+
+if True:
+    print("Uploading...", end="")
+
+    login = "mrAndersen"
+    password = "matrixx1s"
+
+    g = github.Github("mrAndersen", "matrixx1s")
+    niceTime = time.strftime("%H:%M:%S %d-%B", time.localtime())
+    release = g.get_user().get_repo("cppForestCorporation").create_git_release(tag="early-alpha-" + timeString, message="Automated release", name=niceTime)
+
+    binaryData = open(releaseDirectory + ".zip", 'rb').read()
+
+    headers = {
+        'Authorization': "Basic " + base64.b64encode((login + ":" + password).encode("utf-8")).decode("utf-8").replace('\n', ''),
+        'Content-Type': 'application/zip'
+    }
+
+    url = release.upload_url.replace("{?name,label}", "") + "?name=" + releaseDirectory + ".zip"
+    r = requests.post(url, headers=headers, data=binaryData)
+
+    shutil.rmtree(releaseDirectory + ".zip")
+
+    print("\r Uploading... Done\n")
+    print("\r" + release.html_url)
+    print("\n")
